f2e36c90a25da673ae9602e58ee8c974bb2a2ef9
ziim <59916608+sinkkkk@users.noreply.github.com>
2024-04-20 18:49:37 +0300

    Update logic

--- commiter.sh
+++ commiter.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Change directory to the location of the Git repository
 cd /home/siim/Music/greenwa-master/sein
 
-# Generate a random number
 number=$RANDOM
 time=$(date "+%Y-%m-%d %T")
 
-# Check if the random number is even
 if [ $(($number % 2)) -eq 0 ]; then
-    # Append the result to a text file
-    echo "[$(date "+%Y-%m-%d %T")] I rolled $number" >> results.txt
-
-    # Add the text file to the Git staging area
+    echo "[$(date "+%Y-%m-%d %T")] Big change nbr:$number" >> results.txt
     git add results.txt
-
-    # Commit the changes with a message
     git commit -m "I rolled $number!"
-
-    # Push the changes to the remote repository
     git push
 fi
 
