70efa12eb77291a4ffaaf798ab2bcf5e07c05a35
ziim <59916608+sinkkkk@users.noreply.github.com>
2024-04-20 18:56:15 +0300

    Update logic

--- automate.sh
+++ automate.sh
@@ -6,9 +6,9 @@ number=$RANDOM
 time=$(date "+%Y-%m-%d %T")
 
 if [ $(($number % 2)) -eq 0 ]; then
-    echo "[$(date "+%Y-%m-%d %T")] Big change nbr:$number" >> results.txt
+    echo "[$(date "+%Y-%m-%d %T")] Big change nbr:$number" >> log.txt
     git add results.txt
-    git commit -m "I rolled $number!"
+    git commit -m "Big change nbr:$number!"
     git push
 fi
 
